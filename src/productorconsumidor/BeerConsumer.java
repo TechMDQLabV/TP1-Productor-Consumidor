f304e2c41109ccaf922f354f627902d7e5e8fc7c
TechMDQLabV <37402181+TechMDQLabV@users.noreply.github.com>
2018-03-20 10:45:15 -0300

    Add files via upload

---
+++ src/productorconsumidor/BeerConsumer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productorconsumidor;
+
+/**
+ *
+ * @author TechMDQ
+ */
+public class BeerConsumer implements Runnable {
+
+    private final BeerHouse contenedor;
+    private final int idconsumidor;
+
+    public BeerConsumer(BeerHouse contenedor, int idconsumidor) {
+        this.contenedor = contenedor;
+        this.idconsumidor = idconsumidor;
+    }
+
+    @Override
+    /**
+     * Implementación de la hebra
+     */
+    public void run() {
+        while (Boolean.TRUE) {
+            System.out.println("El consumidor " + idconsumidor + " consume: " + contenedor.get());
+        }
+    }
+
+}
---
+++ src/productorconsumidor/BeerHouse.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productorconsumidor;
+
+/**
+ *
+ * @author TechMDQ
+ */
+public class BeerHouse {
+
+    private int contenido;
+    private boolean contenedorlleno = Boolean.FALSE;
+
+    public int getContenido() {
+        return contenido;
+    }
+
+    public synchronized int get() {
+        while (!contenedorlleno) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println("Contenedor: Error en get -> " + e.getMessage());
+            }
+        }
+        contenedorlleno = Boolean.FALSE;
+        notify();
+        return contenido;
+    }
+
+    public synchronized void put(int value) {
+        while (contenedorlleno) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println("Contenedor: Error en put -> " + e.getMessage());
+            }
+        }
+        contenido = value;
+        contenedorlleno = Boolean.TRUE;
+        notify();
+    }
+}
